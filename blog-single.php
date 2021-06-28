60a40a36b21d155f465df9601f8142173b55fc89
webmaster978 <jtjoel11@gmail.com>
2021-06-28 16:46:41 +0200

    presque la fin du blogs

--- blog-single.php
+++ blog-single.php
@@ -47,103 +47,18 @@ $r = $db->query("SELECT * FROM blog  ORDER BY id desc LIMIT 3");
                     <p>
                         <img src="admin/blogs/<?= $g['avatar']; ?>" alt="" class="img-fluid">
                     </p>
-                    <h2 class="mb-3">Incorporation is A Big Milestone for Your Business</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eius mollitia suscipit,
-                        quisquam doloremque distinctio perferendis et doloribus unde architecto optio laboriosam porro
-                        adipisci sapiente officiis nemo accusamus ad praesentium? Esse minima nisi et. Dolore
-                        perferendis, enim praesentium omnis, iste doloremque quia officia optio deserunt molestiae
-                        voluptates soluta architecto tempora.</p>
-                    <p>Molestiae cupiditate inventore animi, maxime sapiente optio, illo est nemo veritatis repellat
-                        sunt doloribus nesciunt! Minima laborum magni reiciendis qui voluptate quisquam voluptatem
-                        soluta illo eum ullam incidunt rem assumenda eveniet eaque sequi deleniti tenetur dolore amet
-                        fugit perspiciatis ipsa, odit. Nesciunt dolor minima esse vero ut ea, repudiandae suscipit!</p>
-                    <h2 class="mb-3 mt-5">#2. Creative WordPress Themes</h2>
-                    <p>Temporibus ad error suscipit exercitationem hic molestiae totam obcaecati rerum, eius aut, in.
-                        Exercitationem atque quidem tempora maiores ex architecto voluptatum aut officia doloremque.
-                        Error dolore voluptas, omnis molestias odio dignissimos culpa ex earum nisi consequatur quos
-                        odit quasi repellat qui officiis reiciendis incidunt hic non? Debitis commodi aut, adipisci.</p>
+                    <h2 class="mb-3"><?= $g['titre']; ?></h2>
                     <p>
-                        <img src="images/image_3.jpg" alt="" class="img-fluid">
-                    </p>
-                    <p>Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. Nihil explicabo
-                        quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa voluptate maiores officiis quis
-                        vel ab consectetur suscipit veritatis nulla quos quia aspernatur perferendis, libero sint.
-                        Error, velit, porro. Deserunt minus, quibusdam iste enim veniam, modi rem maiores.</p>
-                    <div class="tag-widget post-tag-container mb-5 mt-5">
-                        <div class="tagcloud">
-                            <a href="#" class="tag-cloud-link">finance</a>
-                            <a href="#" class="tag-cloud-link">funds</a>
-                            <a href="#" class="tag-cloud-link">invest</a>
-                            <a href="#" class="tag-cloud-link">business</a>
-                        </div>
-                    </div>
+                        <?= $g['contenue']; ?>
 
-                    <div class="about-author d-flex p-4 bg-light rounded">
-                        <div class="bio me-md-4 img" style="background-image: url(images/person_1.jpg);"></div>
-                        <div class="desc">
-                            <h3>John Henderson</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem
-                                necessitatibus voluptate quod mollitia delectus aut, sunt placeat nam vero culpa
-                                sapiente consectetur similique, inventore eos fugit cupiditate numquam!</p>
-                        </div>
-                    </div>
+
+
+
                     <div class="pt-5 mt-5">
-                        <h3 class="mb-5" style="font-size: 34px;">6 Comments</h3>
-                        <ul class="comment-list">
-                            <li class="comment">
-                                <div class="vcard bio">
-                                    <img src="images/person_1.jpg" alt="Image placeholder">
-                                </div>
-                                <div class="comment-body">
-                                    <h3>John Doe</h3>
-                                    <div class="meta">January 27, 2021 at 7:20am</div>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum
-                                        necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim
-                                        sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                                    <p><a href="#" class="reply">Reply</a></p>
-                                </div>
-                            </li>
 
 
-                        </ul>
 
-                        <div class="comment-form-wrap pt-5">
-                            <h3 class="mb-5" style="font-size: 34px;">Leave a comment</h3>
-                            <form action="#" class="p-4 p-lg-5 comment-form">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="name">Name *</label>
-                                            <input type="text" class="form-control" id="name">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="email">Email *</label>
-                                            <input type="email" class="form-control" id="email">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label for="website">Website</label>
-                                            <input type="url" class="form-control" id="website">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label for="message">Message</label>
-                                            <textarea name="" id="message" cols="30" rows="10"
-                                                class="form-control"></textarea>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <input type="submit" value="Post Comment" class="btn py-3 px-4 btn-primary">
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+
                     </div>
                 </div>
                 <?php } ?>
@@ -200,20 +115,7 @@ $r = $db->query("SELECT * FROM blog  ORDER BY id desc LIMIT 3");
 
 
                     </div>
-                    <div class="sidebar-box">
-                        <h3>Tag Cloud</h3>
-                        <div class="tagcloud">
-                            <a href="#" class="tag-cloud-link">finance</a>
-                            <a href="#" class="tag-cloud-link">fund</a>
-                            <a href="#" class="tag-cloud-link">consult</a>
-                            <a href="#" class="tag-cloud-link">business</a>
-                            <a href="#" class="tag-cloud-link">funding</a>
-                            <a href="#" class="tag-cloud-link">money</a>
-                            <a href="#" class="tag-cloud-link">invest</a>
-                            <a href="#" class="tag-cloud-link">plan</a>
-                            <a href="#" class="tag-cloud-link">tax</a>
-                        </div>
-                    </div>
+
                     <div class="sidebar-box">
                         <h3>Paragraph</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem
